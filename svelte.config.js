b438f62e8d82fb9eb9e1b4d75a43644db702e5b5
flloschy <floschy.email@gmail.com>
2024-02-28 21:15:42 +0100

    svelte config tweaks

--- svelte.config.js
+++ svelte.config.js
@@ -26,13 +26,15 @@ const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
 	extensions: ['.svelte', '.md'],
-	preprocess: [vitePreprocess(), mdsvex(mdsvexOptions)],
+	preprocess: [vitePreprocess({ postcss:true }), mdsvex(mdsvexOptions)],
 
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter({
+			preprocess: true
+		})
 	}
 };
 
